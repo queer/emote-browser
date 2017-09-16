56e37d6699a9956e305b9e20fabb4a2b0fb8056a
Amy Null <amy@willnot.date>
2017-09-16 10:25:58 -0400

    idk why I thought that 60k+ emotes wouldn't lag like hell...

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import *
+import json
 import os
+
 import psycopg2
+from flask import *
 
 app = Flask(__name__)
 db_username = os.getenv("POSTGRES_USERNAME")
@@ -10,16 +12,34 @@ db_host = "postgres"
 
 
 # noinspection SqlResolve
-@app.route('/')
-def emote_list():
+@app.route('/search/<emote>')
+def search(emote):
+    emote = str(emote)
+    if len(emote) < 3 or len(emote) > 32:
+        return "Search must fulfill 3 < search < 32."
+
     conn = psycopg2.connect(dbname=db_name, user=db_username, password=db_password, host=db_host)
     c = conn.cursor()
     c.execute('''SELECT name, id, row_number() OVER (PARTITION BY emotes.name ORDER BY emotes.guild) AS 
                  discrim FROM emotes ORDER BY name, discrim ASC;''')
-    render = render_template('index.html', emotes=c.fetchall())
+    res = json.dumps(c.fetchall())
     c.close()
     conn.close()
+    return res;
+
+
+# noinspection SqlResolve
+@app.route('/')
+def emote_list():
+    # conn = psycopg2.connect(dbname=db_name, user=db_username, password=db_password, host=db_host)
+    # c = conn.cursor()
+    # c.execute('''SELECT name, id, row_number() OVER (PARTITION BY emotes.name ORDER BY emotes.guild) AS
+    #             discrim FROM emotes ORDER BY name, discrim ASC;''')
+    render = render_template('index.html')
+    # c.close()
+    # conn.close()
     return render
+
 
 if __name__ == "__main__":
     app.debug = False
